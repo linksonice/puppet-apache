5c7058b5705d1b85e35716e05801d564705cdcdd
root <root@adoicin1c.mylabserver.com>
2020-01-03 17:22:36 +0000

    Add params class for multi-distro support

---
+++ manifests/params.pp
@@ -0,0 +1,14 @@
+# @summary A short summary of the purpose of this class
+#  operating system related variables and others
+class apache::params {
+  $install_ensure = 'present'
+
+  case $::osfamily {
+    'RedHat': {
+      $install_name = 'httpd'
+    }
+    'Debian': {
+      $install_name = 'apache2'
+    }
+  }
+}
