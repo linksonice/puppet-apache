bf51c5488af6f83b4cb1f2515063e17f18c14dfc
linksonice <root@adoicin1c.mylabserver.com>
2020-03-04 17:41:20 +0000

    Added a service class

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,10 +5,13 @@ class apache (
   String $install_ensure,
   String $config_ensure,
   String $config_path,
+  String $service_name,
 ) {
   include apache::install
   include apache::config
+  include apache::service
 
   Class['::apache::install']
   -> Class['::apache::config']
+  ~> Class['::apache::service']
 }
