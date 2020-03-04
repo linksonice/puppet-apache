b569e7fa95e78656f952594984b874829737c9e1
root <root@adoicin1c.mylabserver.com>
2020-03-04 16:49:38 +0000

    Added config class to manage configuration files

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,12 @@
 class apache (
   String $install_name,
   String $install_ensure,
+  String $config_ensure,
+  String $config_path,
 ) {
   include apache::install
+  include apache::config
+
+  Class['::apache::install']
+  -> Class['::apache::config']
 }
